1f5aa1b3f3915839d09e104f55d2440eab2357bb
Anton Demchenko <d.flier@yandex.ru>
2020-04-06 16:01:21 +0200

    Clean train.py by flake8

--- train.py
+++ train.py
@@ -4,12 +4,10 @@ import os
 import numpy as np
 import soundfile as sf
 import tensorflow as tf
+from keras import backend as K
 from keras.callbacks import ModelCheckpoint
 from keras.optimizers import RMSprop
 from keras.utils import to_categorical
-from keras import backend as K
-
-K.clear_session()
 
 import conf
 
@@ -21,18 +19,22 @@ def batchGenerator(conf, fact_amp, out_dim):
 
 
 def create_batches_rnd(conf, fact_amp, out_dim):
-    """ 
-    Initialization of the minibatch 
+    """
+    Initialization of the minibatch
     (batch_size, [0=>x_t, 1=>x_t+N, 1=>random_samp])
     """
     sig_batch = np.zeros([conf.batch_size, conf.wlen])
     lab_batch = []
     snt_id_arr = np.random.randint(conf.N_snt, size=conf.batch_size)
-    rand_amp_arr = np.random.uniform(1.0 - fact_amp, 1 + fact_amp, conf.batch_size)
+    rand_amp_arr = np.random.uniform(
+        1.0 - fact_amp,
+        1 + fact_amp,
+        conf.batch_size
+    )
     for i in range(conf.batch_size):
         # select a random sentence from the list
         fname = conf.data_folder + conf.wav_lst[snt_id_arr[i]]
-        with tf.io.gfile.GFile(fname, "rb") as f:
+        with tf.io.gfile.GFile(fname, 'rb') as f:
             [signal, fs] = sf.read(io.BytesIO(f.read()))
         # accesing to a random chunk
         snt_len = signal.shape[0]
@@ -48,6 +50,7 @@ def create_batches_rnd(conf, fact_amp, out_dim):
 
 
 def main():
+    K.clear_session()
     # np.random.seed(seed)
     # from tensorflow import set_random_seed
     # set_random_seed(seed)
@@ -59,21 +62,23 @@ def main():
     model = getModel(input_shape, out_dim)
     optimizer = RMSprop(lr=conf.lr, rho=0.9, epsilon=1e-8)
     model.compile(
-        loss="categorical_crossentropy", optimizer=optimizer, metrics=["accuracy"]
+        loss='categorical_crossentropy',
+        optimizer=optimizer,
+        metrics=['accuracy']
     )
 
-    checkpoints_path = os.path.join(conf.output_folder, "checkpoints")
+    checkpoints_path = os.path.join(conf.output_folder, 'checkpoints')
     if not os.path.exists(checkpoints_path):
         os.makedirs(checkpoints_path)
 
     checkpointer = ModelCheckpoint(
-        filepath=os.path.join(checkpoints_path, "SincNet.hdf5"),
+        filepath=os.path.join(checkpoints_path, 'SincNet.hdf5'),
         verbose=1,
         save_best_only=False,
     )
     callbacks = [checkpointer]
 
-    if conf.pt_file != "none":
+    if conf.pt_file != 'none':
         model.load_weights(conf.pt_file)
 
     train_generator = batchGenerator(conf, 0.2, out_dim)
@@ -86,5 +91,5 @@ def main():
     )
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()

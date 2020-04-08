434dc0aeaff5bd67586c13e1f2dba727513ee25d
Anton Demchenko <d.flier@yandex.ru>
2020-04-08 15:46:58 +0200

    Add data_loader module

---
+++ data_loader.py
@@ -0,0 +1,59 @@
+import io
+
+import numpy as np
+import soundfile as sf
+import tensorflow as tf
+from tensorflow.keras.utils import to_categorical
+
+
+def read_wav(path):
+    with tf.io.gfile.GFile(path, 'rb') as f:
+        signal, _ = sf.read(io.BytesIO(f.read()))
+        return signal
+
+
+def get_label(cfg, path):
+    label = cfg.lab_dict[path]
+    return to_categorical(label, num_classes=cfg.out_dim)
+
+
+def get_training_sample(cfg, path):
+    full_path = cfg.data_folder + path
+    signal = read_wav(full_path)
+    chunk_begin = np.random.randint(signal.shape[0] - cfg.wlen)
+    chunk = signal[chunk_begin : chunk_begin + cfg.wlen]
+    amp = np.random.uniform(1.0 - cfg.fact_amp, 1.0 + cfg.fact_amp)
+    chunk = chunk * amp
+    label = get_label(cfg, path)
+    yield chunk.reshape((chunk.shape[0], 1)), label
+
+
+def get_testing_samples(cfg, path):
+    full_path = cfg.data_folder + path
+    signal = read_wav(full_path)
+    label = get_label(cfg, path)
+    for chunk_begin in range(0, signal.shape[0] - cfg.wlen, cfg.wshift):
+        chunk = signal[chunk_begin : chunk_begin + cfg.wlen]
+        yield chunk.reshape((chunk.shape[0], 1)), label
+
+
+def sample_reader(cfg, path_list, get_samples):
+    for path in path_list:
+        yield from get_samples(cfg, path)
+
+
+def make_dataset(cfg, path_list, for_train=True):
+    get_samples = get_training_sample if for_train else get_testing_samples
+
+    def get_generator():
+        return sample_reader(cfg, path_list, get_samples)
+
+    dataset = tf.data.Dataset.from_generator(
+        get_generator,
+        (tf.float64, tf.int64),
+        (tf.TensorShape([cfg.wlen, 1]), tf.TensorShape([cfg.out_dim])),
+    )
+    if for_train:
+        dataset = dataset.shuffle(256).repeat()
+    dataset = dataset.batch(cfg.batch_size).prefetch(10)
+    return dataset
--- train.py
+++ train.py
@@ -1,43 +1,12 @@
-import io
 import os
 
-import numpy as np
-import soundfile as sf
-import tensorflow as tf
 from tensorflow.keras import backend as K
 from tensorflow.keras.callbacks import ModelCheckpoint
 from tensorflow.keras.optimizers import RMSprop
-from tensorflow.keras.utils import to_categorical
 
 from config import read_config
+from data_loader import make_dataset
 from sincnet import SincNetModel
-
-
-def read_wav(path):
-    with tf.io.gfile.GFile(path, 'rb') as f:
-        signal, _ = sf.read(io.BytesIO(f.read()))
-        return signal
-
-
-def get_label(path, cfg):
-    label = cfg.lab_dict[path]
-    return to_categorical(label, num_classes=cfg.out_dim)
-
-
-def get_sample(path, cfg):
-    full_path = cfg.data_folder + path
-    signal = read_wav(full_path)
-    chunk_begin = np.random.randint(signal.shape[0] - cfg.wlen)
-    signal = signal[chunk_begin : chunk_begin + cfg.wlen]
-    amp = np.random.uniform(1.0 - cfg.fact_amp, 1.0 + cfg.fact_amp)
-    signal = signal * amp
-    label = get_label(path, cfg)
-    return signal.reshape((signal.shape[0], 1)), label
-
-
-def sample_reader(cfg):
-    for path in cfg.train_list:
-        yield get_sample(path, cfg)
 
 
 def main():
@@ -68,11 +37,7 @@ def main():
     if cfg.pt_file != 'none':
         model.load_weights(cfg.pt_file)
 
-    train_dataset = tf.data.Dataset.from_generator(
-        lambda: sample_reader(cfg), 
-        (tf.float64, tf.int64), 
-        (tf.TensorShape([cfg.wlen, 1]), tf.TensorShape([cfg.out_dim])),
-    ).shuffle(256).repeat().batch(cfg.batch_size).prefetch(2)
+    train_dataset = make_dataset(cfg, cfg.train_list, for_train=True)
     model.fit(
         train_dataset,
         steps_per_epoch=cfg.N_batches,

a94a87948f160ef74c8af5c164781efb9b061a7c
Anton Demchenko <d.flier@yandex.ru>
2020-04-18 15:18:22 +0000

    Add sample accuracy in testing

--- test.py
+++ test.py
@@ -9,12 +9,20 @@ from sincnet import SincNetClassifierFactory
 def test(cfg, model, data_loader, path_list):
     dataset = data_loader.make_test_iterable(path_list)
     path_to_prediction_sum = dict()
+    samples_count = 0
+    error_count = 0
     for path_batch, signal_batch in tqdm(dataset):
+        samples_count += len(path_batch)
         prediction_batch = model.predict(signal_batch)
         for path, prediction in zip(path_batch, prediction_batch):
             if path not in path_to_prediction_sum:
                 path_to_prediction_sum[path] = np.zeros([cfg.n_classes])
             path_to_prediction_sum[path] += prediction
+            predicted_label = np.argmax(prediction)
+            expected_label = cfg.lab_dict[path]
+            if expected_label != predicted_label:
+                error_count += 1
+    accuracy_sample = 1 - (error_count / samples_count)
     error_count = 0
     for path in path_list:
         prediction_sum = path_to_prediction_sum[path]
@@ -22,8 +30,8 @@ def test(cfg, model, data_loader, path_list):
         predicted_label = np.argmax(prediction_sum)
         if expected_label != predicted_label:
             error_count += 1
-    accuracy = 1 - (error_count / len(path_list))
-    return accuracy
+    accuracy_path = 1 - (error_count / len(path_list))
+    return accuracy_sample, accuracy_path
 
 
 def main():

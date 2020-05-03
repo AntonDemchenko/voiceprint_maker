cd6a65276a5a0c810fe1ac622d753fdb9baf72ad
Anton Demchenko <d.flier@yandex.ru>
2020-05-03 06:01:05 +0000

    Downgrade to old sincnet creating

--- sincnet.py
+++ sincnet.py
@@ -7,251 +7,184 @@ from tensorflow.keras import backend as K
 from tensorflow.keras import layers
 
 
-def to_mel(hz):
-    return 2595 * np.log10(1 + hz / 700)
+class SincConv1D(tf.keras.layers.Layer):
+    def __init__(self, N_filt, Filt_dim, fs, **kwargs):
+        self.N_filt = N_filt
+        self.Filt_dim = Filt_dim
+        self.fs = fs
 
-
-def to_hz(mel):
-    return 700 * (10 ** (mel / 2595) - 1)
-
-
-class SincConv(layers.Layer):
-    def __init__(
-        self,
-        out_channels,
-        kernel_size,
-        sample_rate=16000,
-        in_channels=1,
-        stride=1,
-        padding='valid',
-        dilation=1,
-        min_low_hz=50,
-        min_band_hz=50
-    ):
-        if in_channels != 1:
-            raise ValueError('SinConv supports only one input channel')
-
-        self.in_channels = in_channels
-        self.out_channels = out_channels
-        self.kernel_size = kernel_size
-        if kernel_size % 2 == 0:
-            self.kernel_size = self.kernel_size + 1
-
-        self.stride = stride
-        self.padding = padding
-        self.dilation = dilation
-        self.sample_rate = sample_rate
-        self.min_low_hz = min_low_hz
-        self.min_band_hz = min_band_hz
-
-        super().__init__()
+        super(SincConv1D, self).__init__(**kwargs)
 
     def get_config(self):
         config = super().get_config().copy()
         config.update({
-            'in_channels': self.in_channels,
-            'out_channels': self.out_channels,
-            'kernel_size': self.kernel_size,
-            'stride': self.stride,
-            'padding': self.padding,
-            'dilation': self.dilation,
-            'sample_rate': self.sample_rate,
-            'min_low_hz': self.min_low_hz,
-            'min_band_hz': self.min_band_hz
+            'N_filt': self.N_filt,
+            'Filt_dim': self.Filt_dim,
+            'fs': self.fs
         })
         return config
 
     def build(self, input_shape):
-        # initialize filterbanks such that they are equally spaced in Mel scale
-        low_hz = 30
-        high_hz = 0.5 * self.sample_rate - (self.min_low_hz + self.min_band_hz)
-
-        mel = np.linspace(
-            to_mel(low_hz),
-            to_mel(high_hz),
-            self.out_channels + 1
-        )
-        hz = to_hz(mel)
-
-        # filter lower frequency (out_channels, in_channels)
-        self.low_hz_ = self.add_weight(
-            name='low_hz',
-            shape=(self.out_channels, 1),
+        # The filters are trainable parameters.
+        self.filt_b1 = self.add_weight(
+            name='filt_b1',
+            shape=(self.N_filt,),
             initializer='uniform',
             trainable=True
         )
-
-        # filter frequency band (out_channels, in_channels)
-        self.band_hz_ = self.add_weight(
-            name='band_hz',
-            shape=(self.out_channels, 1),
+        self.filt_band = self.add_weight(
+            name='filt_band',
+            shape=(self.N_filt,),
             initializer='uniform',
             trainable=True,
         )
-        self.set_weights([
-            hz[:-1].reshape([-1, 1]),
-            np.diff(hz).reshape([-1, 1])
-        ])
 
-        linspace = np.linspace(
-            0,
-            self.kernel_size // 2,
-            self.kernel_size // 2
-        )
-        self.window_ = 0.54 - 0.46 * np.cos(2 * np.pi * linspace / self.kernel_size)
+        # Mel Initialization of the filterbanks
+        low_freq_mel = 80
+        high_freq_mel = 2595 * np.log10(1 + (self.fs / 2) / 700)  # Convert Hz to Mel
+        mel_points = np.linspace(
+            low_freq_mel, high_freq_mel, self.N_filt
+        )  # Equally spaced in Mel scale
+        f_cos = 700 * (10 ** (mel_points / 2595) - 1)  # Convert Mel to Hz
+        b1 = np.roll(f_cos, 1)
+        b2 = np.roll(f_cos, -1)
+        b1[0] = 30
+        b2[-1] = (self.fs / 2) - 100
+        self.freq_scale = self.fs * 1.0
+        self.set_weights([b1 / self.freq_scale, (b2 - b1) / self.freq_scale])
 
-        n = 0.5 * (self.kernel_size - 1)
-        self.n_ = 2 * np.pi * np.arange(-n, 0).reshape([1, -1]) / self.sample_rate
-        self.n_ = self.n_.astype(np.float32)
+        super(SincConv1D, self).build(input_shape)  # Be sure to call this at the end
 
-        super().build(input_shape)
+    def call(self, x):
+        # filters = K.zeros(shape=(N_filt, Filt_dim))
 
-    def __call__(self, x):
-        low = self.min_low_hz + tf.math.abs(self.low_hz_)
-        high = tf.clip_by_value(
-            low + self.min_band_hz + tf.math.abs(self.band_hz_),
-            self.min_low_hz,
-            0.5 * self.sample_rate
-        )
-        band = (high - low)[:, 0]
-
-        f_times_t_low = tf.linalg.matmul(low, self.n_)
-        f_times_t_high = tf.linalg.matmul(high, self.n_)
-
-        band_pass_left = self.window_ * ((tf.sin(f_times_t_high) - tf.sin(f_times_t_low) / (0.5 * self.n_)))
-        band_pass_center = tf.reshape(2 * band, [-1, 1])
-        band_pass_right = tf.reverse(band_pass_left, axis=[1])
-        band_pass = tf.concat([band_pass_left, band_pass_center, band_pass_right], axis=1)
-        band_pass = band_pass / (2 * band[:, None])
-
-        self.filters = tf.reshape(
-            band_pass,
-            [self.kernel_size, self.in_channels, self.out_channels]
+        # Get beginning and end frequencies of the filters.
+        min_freq = 50.0
+        min_band = 50.0
+        filt_beg_freq = K.abs(self.filt_b1) + min_freq / self.freq_scale
+        filt_end_freq = filt_beg_freq + (
+            K.abs(self.filt_band) + min_band / self.freq_scale
         )
 
-        return K.conv1d(
-            x,
-            self.filters,
-            self.stride,
-            self.padding,
-            dilation_rate=self.dilation
+        # Filter window (hamming).
+        n = np.linspace(0, self.Filt_dim, self.Filt_dim)
+        window = 0.54 - 0.46 * np.cos(2 * math.pi * n / self.Filt_dim)
+        # window = K.cast(window, 'float32')
+        # window = tf.Variable(window, name='sincnet_window', trainable=False)
+
+        # TODO what is this?
+        t_right_linspace = np.linspace(
+            1, (self.Filt_dim - 1) / 2, int((self.Filt_dim - 1) / 2), dtype=np.float32
         )
+        t_right = np.float32(t_right_linspace / self.fs)
+        # t_right = tf.Variable(t_right, name='sincnet_t_right', trainable=False, dtype=tf.float32)
+
+        # Compute the filters.
+        output_list = []
+        for i in range(self.N_filt):
+            low_pass1 = (
+                2 * filt_beg_freq[i] * sinc(filt_beg_freq[i] * self.freq_scale, t_right)
+            )
+            low_pass2 = (
+                2 * filt_end_freq[i] * sinc(filt_end_freq[i] * self.freq_scale, t_right)
+            )
+            band_pass = low_pass2 - low_pass1
+            band_pass = band_pass / K.max(band_pass)
+            output_list.append(band_pass * window)
+        filters = K.stack(output_list)  # (80, 251)
+        filters = K.transpose(filters)  # (251, 80)
+        filters = K.reshape(
+            filters, (self.Filt_dim, 1, self.N_filt)
+        )  # (251,1,80) in TF: (filter_width, in_channels, out_channels) in PyTorch (out_channels, in_channels, filter_width)
+
+        """
+        Given an input tensor of shape [batch, in_width, in_channels] if data_format is "NWC",
+        or [batch, in_channels, in_width] if data_format is "NCW", and a filter / kernel tensor of shape [filter_width, in_channels, out_channels],
+        this op reshapes the arguments to pass them to conv2d to perform the equivalent convolution operation.
+        Internally, this op reshapes the input tensors and invokes tf.nn.conv2d. For example, if data_format does not start with "NC",
+        a tensor of shape [batch, in_width, in_channels] is reshaped to [batch, 1, in_width, in_channels], and the filter is reshaped to
+        [1, filter_width, in_channels, out_channels]. The result is then reshaped back to [batch, out_width, out_channels]
+        (where out_width is a function of the stride and padding as in conv2d) and returned to the caller.
+        """
+
+        out = K.conv1d(x, kernel=filters)
+
+        return out
 
     def compute_output_shape(self, input_shape):
         new_size = conv_utils.conv_output_length(
-            input_shape[1],
-            self.kernel_size,
-            padding=self.padding,
-            stride=self.stride,
-            dilation=self.dilation
+            input_shape[1], self.Filt_dim, padding='valid', stride=1, dilation=1
         )
-        return (input_shape[0],) + (new_size,) + (self.out_channels,)
+        return (input_shape[0],) + (new_size,) + (self.N_filt,)
+
+
+def sinc(band, t_right):
+    y_right = tf.sin(2 * tf.constant(np.pi, dtype=tf.float32) * band * t_right) / (
+        2 * tf.constant(np.pi, dtype=tf.float32) * band * t_right
+    )
+    # y_left = flip(y_right, 0) TODO remove if useless
+    y_left = K.reverse(y_right, 0)
+    y = K.cast(
+        K.concatenate([y_left, tf.ones(1, dtype=tf.float32), y_right]), 'float32'
+    )
+    return y
 
 
 class SincNetModelFactory:
     def __init__(self, options):
         self.options = options
 
-        self.cnn_batch_norm_input = self.make_batch_norm()\
-            if options.cnn_use_batch_norm_before\
-            else None
-
-        self.cnn_layer_norm_input = self.make_layer_norm()\
-            if options.cnn_use_layer_norm_before\
-            else None
-
-        sinc = SincConv(
-            out_channels=options.cnn_n_filters[0],
-            kernel_size=options.cnn_filter_len[0],
-            sample_rate=options.sample_rate
+        self.sinc_1 = SincConv1D(
+            options.cnn_n_filters[0], options.cnn_filter_len[0], options.sample_rate
         )
+        self.maxpool_1 = layers.MaxPooling1D(pool_size=options.cnn_max_pool_len[0])
+        if options.cnn_use_batch_norm[0]:
+            self.batch_norm_1 = layers.BatchNormalization(momentum=0.95, epsilon=1e-5)
+        if options.cnn_use_layer_norm[0]:
+            self.layer_norm_1 = layers.LayerNormalization(epsilon=1e-6)
+        self.leaky_relu_1 = layers.LeakyReLU(alpha=0.2)
 
-        self.abs = layers.Lambda(lambda x: tf.math.abs(x))
+        self.conv_2 = layers.Conv1D(
+            options.cnn_n_filters[1], options.cnn_filter_len[1], strides=1, padding='valid'
+        )
+        self.maxpool_2 = layers.MaxPooling1D(pool_size=options.cnn_max_pool_len[1])
+        if options.cnn_use_batch_norm[1]:
+            self.batch_norm_2 = layers.BatchNormalization(momentum=0.95, epsilon=1e-5)
+        if options.cnn_use_layer_norm[1]:
+            self.layer_norm_2 = layers.LayerNormalization(epsilon=1e-6)
+        self.leaky_relu_2 = layers.LeakyReLU(alpha=0.2)
 
-        self.conv = [sinc] + [
-            layers.Conv1D(
-                options.cnn_n_filters[i],
-                options.cnn_filter_len[i],
-                strides=1,
-                padding='valid'
-            )
-            for i in range(1, self.options.cnn_n_layers)
-        ]
-        self.maxpool = [
-            layers.MaxPooling1D(pool_size=options.cnn_max_pool_len[i])
-            for i in range(self.options.cnn_n_layers)
-        ]
-        self.cnn_batch_norm = [
-            self.make_batch_norm()
-            if options.cnn_use_batch_norm[i]
-            else None
-            for i in range(self.options.cnn_n_layers)
-        ]
-        self.cnn_layer_norm = [
-            self.make_layer_norm()
-            if options.cnn_use_layer_norm[i]
-            else None
-            for i in range(self.options.cnn_n_layers)
-        ]
-        self.cnn_activations = [
-            self.make_activation(options.cnn_act[i])
-            for i in range(self.options.cnn_n_layers)
-        ]
-        self.cnn_dropout = [
-            layers.Dropout(options.cnn_drop[i])
-            if not np.isclose(options.cnn_drop[i], 0)
-            else None
-            for i in range(self.options.cnn_n_layers)
-        ]
-
+        self.conv_3 = layers.Conv1D(
+            options.cnn_n_filters[2], options.cnn_filter_len[2], strides=1, padding='valid'
+        )
+        self.maxpool_3 = layers.MaxPooling1D(pool_size=options.cnn_max_pool_len[2])
+        if options.cnn_use_batch_norm[2]:
+            self.batch_norm_3 = layers.BatchNormalization(momentum=0.95, epsilon=1e-5)
+        if options.cnn_use_layer_norm[2]:
+            self.layer_norm_3 = layers.LayerNormalization(epsilon=1e-6)
+        self.leaky_relu_3 = layers.LeakyReLU(alpha=0.2)
         self.flatten = layers.Flatten()
 
-        self.fc_batch_norm_input = self.make_batch_norm()\
-            if options.fc_use_batch_norm_before\
-            else None
+        self.dense_4 = layers.Dense(options.fc_size[0])
+        if options.fc_use_batch_norm[0]:
+            self.batch_norm_4 = layers.BatchNormalization(momentum=0.95, epsilon=1e-5)
+        if options.fc_use_layer_norm[0]:
+            self.layer_norm_4 = layers.LayerNormalization(epsilon=1e-6)
+        self.leaky_relu_4 = layers.LeakyReLU(alpha=0.2)
 
-        self.fc_layer_norm_input = self.make_layer_norm()\
-            if options.fc_use_layer_norm_before\
-            else None
+        self.dense_5 = layers.Dense(options.fc_size[1])
+        if options.fc_use_batch_norm[1]:
+            self.batch_norm_5 = layers.BatchNormalization(momentum=0.95, epsilon=1e-5)
+        if options.fc_use_layer_norm[1]:
+            self.layer_norm_5 = layers.LayerNormalization(epsilon=1e-6)
+        self.leaky_relu_5 = layers.LeakyReLU(alpha=0.2)
 
-        self.dense = [
-            layers.Dense(options.fc_size[i])
-            for i in range(self.options.fc_n_layers)
-        ]
-        self.fc_batch_norm = [
-            self.make_batch_norm()
-            if options.fc_use_batch_norm[i]
-            else None
-            for i in range(self.options.fc_n_layers)
-        ]
-        self.fc_layer_norm = [
-            self.make_layer_norm()
-            if options.fc_use_layer_norm[i]
-            else None
-            for i in range(self.options.fc_n_layers)
-        ]
-        self.fc_activations = [
-            self.make_activation(options.fc_act[i])
-            for i in range(self.options.fc_n_layers)
-        ]
-        self.fc_dropout = [
-            layers.Dropout(options.fc_drop[i])
-            if not np.isclose(options.fc_drop[i], 0)
-            else None
-            for i in range(self.options.fc_n_layers)
-        ]
-
-    def make_layer_norm(self):
-        return layers.LayerNormalization(epsilon=1e-6)
-
-    def make_batch_norm(self):
-        return layers.BatchNormalization(momentum=0.95, epsilon=1e-5)
-
-    def make_activation(self, act):
-        if act == 'leaky_relu':
-            return layers.LeakyReLU(alpha=0.2)
-        if act == 'relu':
-            return layers.ReLU()
+        self.dense_6 = layers.Dense(options.fc_size[2])
+        if options.fc_use_batch_norm[2]:
+            self.batch_norm_6 = layers.BatchNormalization(momentum=0.95, epsilon=1e-5)
+        if options.fc_use_layer_norm[2]:
+            self.layer_norm_6 = layers.LayerNormalization(epsilon=1e-6)
+        self.leaky_relu_6 = layers.LeakyReLU(alpha=0.2)
 
     def get_prediction(self, x):
         raise NotImplementedError
@@ -259,43 +192,52 @@ class SincNetModelFactory:
     def create(self):
         inputs = layers.Input(self.options.input_shape)
 
-        x = inputs
-        if self.cnn_batch_norm_input:
-            x = self.cnn_batch_norm_input(x)
-        if self.cnn_layer_norm_input:
-            x = self.cnn_layer_norm_input(x)
+        x = self.sinc_1(inputs)
 
-        self.conv[0].build(self.options.input_shape) # SincConv build is not called automatically
+        x = self.maxpool_1(x)
+        if self.options.cnn_use_batch_norm[0]:
+            x = self.batch_norm_1(x)
+        if self.options.cnn_use_layer_norm[0]:
+            x = self.layer_norm_1(x)
+        x = self.leaky_relu_1(x)
 
-        for i in range(self.options.cnn_n_layers):
-            x = self.conv[i](x)
-            if i == 0 and self.cnn_layer_norm[i]:
-                x = self.abs(x)
-            x = self.maxpool[i](x)
-            if self.cnn_batch_norm[i]:
-                x = self.cnn_batch_norm[i](x)
-            if self.cnn_layer_norm[i]:
-                x = self.cnn_layer_norm[i](x)
-            x = self.cnn_activations[i](x)
-            if self.cnn_dropout[i]:
-                x = self.cnn_dropout[i](x)
+        x = self.conv_2(x)
+        x = self.maxpool_2(x)
+        if self.options.cnn_use_batch_norm[1]:
+            x = self.batch_norm_2(x)
+        if self.options.cnn_use_layer_norm[1]:
+            x = self.layer_norm_2(x)
+        x = self.leaky_relu_2(x)
 
+        x = self.conv_3(x)
+        x = self.maxpool_3(x)
+        if self.options.cnn_use_batch_norm[2]:
+            x = self.batch_norm_3(x)
+        if self.options.cnn_use_layer_norm[2]:
+            x = self.layer_norm_3(x)
+        x = self.leaky_relu_3(x)
         x = self.flatten(x)
 
-        if self.fc_batch_norm_input:
-            x = self.fc_batch_norm_input(x)
-        if self.fc_layer_norm_input:
-            x = self.fc_layer_norm_input(x)
+        x = self.dense_4(x)
+        if self.options.fc_use_batch_norm[0]:
+            x = self.batch_norm_4(x)
+        if self.options.fc_use_layer_norm[0]:
+            x = self.layer_norm_4(x)
+        x = self.leaky_relu_4(x)
 
-        for i in range(self.options.fc_n_layers):
-            x = self.dense[i](x)
-            if self.fc_batch_norm[i]:
-                x = self.fc_batch_norm[i](x)
-            if self.fc_layer_norm[i]:
-                x = self.fc_layer_norm[i](x)
-            x = self.fc_activations[i](x)
-            if self.fc_dropout[i]:
-                x = self.fc_dropout[i](x)
+        x = self.dense_5(x)
+        if self.options.fc_use_batch_norm[1]:
+            x = self.batch_norm_5(x)
+        if self.options.fc_use_layer_norm[1]:
+            x = self.layer_norm_5(x)
+        x = self.leaky_relu_5(x)
+
+        x = self.dense_6(x)
+        if self.options.fc_use_batch_norm[2]:
+            x = self.batch_norm_6(x)
+        if self.options.fc_use_layer_norm[2]:
+            x = self.layer_norm_6(x)
+        x = self.leaky_relu_6(x)
 
         prediction = self.get_prediction(x)
 
@@ -306,20 +248,9 @@ class SincNetModelFactory:
 class SincNetClassifierFactory(SincNetModelFactory):
     def __init__(self, options):
         super().__init__(options)
-        self.class_batch_norm = self.make_batch_norm()\
-            if options.class_use_batch_norm_before\
-            else None
-        self.class_layer_norm = self.make_layer_norm()\
-            if options.class_use_layer_norm_before\
-            else None
-        self.class_layer = layers.Dense(self.options.n_classes, activation='softmax')
 
     def get_prediction(self, x):
-        if self.class_batch_norm:
-            x = self.class_batch_norm(x)
-        if self.class_layer_norm:
-            x = self.class_layer_norm(x)
-        x = self.class_layer(x)
+        x = layers.Dense(self.options.n_classes, activation='softmax')(x)
         return x
 
 
